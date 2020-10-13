d3cff6cf8cc5a4db060b3033349df2dbc04fa62e
r4minn <62107150+r4minn@users.noreply.github.com>
2020-10-13 13:39:23 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,27 @@
+
+print("\u001b[35m                                   ▄▄\u001b[35m")
+print("\u001b[35m                                   ██\u001b[35m")
+print("\u001b[35m▀███▄███ ▄█▀██▄ ▀████████▄█████▄ ▀███ ▀████████▄\u001b[35m")
+print("\u001b[35m  ██▀ ▀▀██   ██   ██    ██    ██   ██   ██    ██\u001b[35m")
+print("\u001b[35m  ██     ▄█████   ██    ██    ██   ██   ██    ██\u001b[35m")
+print("\u001b[35m  ██    ██   ██   ██    ██    ██   ██   ██    ██\u001b[35m")
+print("\u001b[35m▄████▄  ▀████▀██▄████  ████  ████▄████▄████  ████▄\u001b[35m")
+print("                                                  ")
+
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+resultat = float(num1) + float(num2)
+resultat2 = float(num1) - float(num2)
+resultat3 = float(num1) * float(num2)
+resultat4 = float(num1) / float(num2)
+
+print(num1 + " + " + num2 + " = " + str(resultat))
+print(num1 + " - " + num2 + " = " + str(resultat2))
+print(num1 + " * " + num2 + " = " + str(resultat3))
+print(num1 + " / " + num2 + " = " + str(resultat4))
+
+print("                                                  ")
+print("Correct?") ; helpful = input("Y/N ")
+
+
